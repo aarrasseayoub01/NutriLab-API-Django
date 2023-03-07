245a73c42e1aa00818713b247ce286582b0f5666
Ayoubon <aarrasseayoub@gmail.com>
2023-03-07 17:16:00 +0100

    Redoing the algorith (AGAIN) for real this time)

---
+++ polls/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.getFood, name='food'),
+    path('post/', views.postFood),
+    path('getFood/', views.food)
+]
